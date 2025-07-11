84b6946fdc40c6248e1d19710f0266c1d9a47c88
harishpandu548 <harishpandu548@gmail.com>
2025-07-12 05:01:10 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  server:{
-    proxy:{
-      "/api":"http://localhost:8000"
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000" 
     }
-  }
+  },
+  build: {
+    outDir: 'dist', 
+  },
+  resolve: {
+    alias: {
+      '@': '/src', 
+    },
+  },
+  base: '/', 
 })
